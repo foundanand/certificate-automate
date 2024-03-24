e178033e1d351a1d0908e830b3b7fdd70153b20d
foundanand <anand.lahoti2021@vitbhopal.ac.in>
2024-03-24 18:00:50 +0530

    Optimised for resuability

--- certificate_mail.py
+++ certificate_mail.py
@@ -20,7 +20,7 @@ def send_mail(EMAIL_ID, PER_NAME, EVENT_NAME, CERTIFICATE_FOLDER, CERTIFICATE):
     msg = MIMEMultipart()
     msg['From'] = gmail_user
     msg['To'] = EMAIL_ID
-    msg['Subject'] = f"Certificate of Participation | FINGENAI Hackathon 💡 | 16th Feburary"
+    msg['Subject'] = "Certificate of Participation"
 
     # Add HTML text to the email body
     html = f"""
@@ -41,19 +41,7 @@ def send_mail(EMAIL_ID, PER_NAME, EVENT_NAME, CERTIFICATE_FOLDER, CERTIFICATE):
         </head>
         <body>
             <p>Dear {PER_NAME},<br><br>
-            We hope this email finds you well. As we wrap up an exhilarating journey of innovation and collaboration at the FinGenAI Hackathon, we wanted to take a moment to extend our heartfelt gratitude for your enthusiastic participation and contribution.<br><br>
-            Your dedication, creativity, and willingness to tackle challenging financial-based problems have truly enriched the experience for everyone involved. The diverse array of ideas and solutions presented has been truly inspiring, showcasing the incredible talent and potential within our community.<br>
-            In recognition of your valuable participation, we are pleased to attach your Certificate of Participation here. This certificate is a token of our appreciation for your commitment and efforts, and we hope it serves as a fond reminder of your accomplishments and the exciting time we spent together.<br><br>
-            In addition to this, we are thrilled to extend our warmest congratulations to our winning team:<br>
-            Team Name: Team 404<br>
-            for their outstanding victory by their exceptional performance, dedication and innovative solution.<br>
-            You will receive the hard copy of your winning certificates in this upcoming week.<br><br>
-            Once again, thank you all for making the FinGenAI Hackathon a remarkable event. We wish you all the best in your future endeavors and hope to cross paths again in the journey of Fintech exploration and advancement.<br><br>
-            <p>Additionally, for those interested in reliving the moments from the event, please find the link to the event photos <a href="https://drive.google.com/drive/folders/1FEYYHELjAaDmHIgB07rm9oKF1VkW4F7L">here</a>.</p> <br><br>
-
-            Warmest Regards,<br>
-            FinTech Club,<br>
-            VIT Bhopal University<br>
+            Add your message here.
             </p>
         </body>
         </html>
@@ -62,11 +50,11 @@ def send_mail(EMAIL_ID, PER_NAME, EVENT_NAME, CERTIFICATE_FOLDER, CERTIFICATE):
     msg.attach(MIMEText(html, 'html'))
 
     # Add QR code attachment to the email
-    qr_code_path = (os.path.join(CERTIFICATE_FOLDER, f"{PER_NAME}_{EVENT_NAME}.pdf"))
-    with open(qr_code_path, 'rb') as f:
-        qr_code = MIMEApplication(f.read(), _subtype='png')
-    qr_code.add_header('content-disposition', 'attachment', filename=os.path.basename(qr_code_path))
-    msg.attach(qr_code)
+    certificate_path = (os.path.join(CERTIFICATE_FOLDER, f"{PER_NAME}_{EVENT_NAME}.pdf"))
+    with open(certificate_path, 'rb') as f:
+        participant_certificate = MIMEApplication(f.read(), _subtype='png')
+    participant_certificate.add_header('content-disposition', 'attachment', filename=os.path.basename(certificate_path))
+    msg.attach(participant_certificate)
 
     # Create the SMTP server and send the email
     server = smtplib.SMTP('smtp.gmail.com', 587)
