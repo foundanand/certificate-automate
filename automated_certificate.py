69b460e732212cc61424599d773e5316f8436671
foundanand <anand.lahoti2021@vitbhopal.ac.in>
2024-03-24 18:00:26 +0530

    Added comments

--- automated_certificate.py
+++ automated_certificate.py
@@ -9,16 +9,22 @@ from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase import pdfmetrics
 from certificate_mail import send_mail
 
-# NAME_FONT = pdfmetrics.registerFont(TTFont('Great Vibes 400', 'greatvibes.ttf'))
-# NAME_FONT = pdfmetrics.registerFont(TTFont('Poppins', 'Poppins Regular 400.ttf'))
+
+#  Download the font in ttf format and place it in the same directory as the script
 NAME_FONT = pdfmetrics.registerFont(TTFont('Book-Antiqua', 'Book-Antiqua.ttf'))
-FILEPATH = "FinGenAI_List_Certificate.xlsx"
-# EMAIL_ID = "anand.lahoti2021@vitbhopal.ac.in"
+
+# Path to the excel file
+FILEPATH = "/path/to/excel/file.xlsx"
+
+# Sample Test case
+# EMAIL_ID = "anand.lahoti15@gmail.com"
 # PER_NAME = "Anand Lahoti"
-CERTIFICATE = "certificate 4.pdf"
-EVENT_NAME  = "FinGenAI"
-CERTIFICATE_FOLDER = f"event_certificate/{EVENT_NAME}"
-FONT_NAME = "Book-Antiqua"
+
+# Certificate Details
+CERTIFICATE = "certificate 4.pdf" # Path to the certificate template
+EVENT_NAME  = "FinGenAI" # Event Name
+CERTIFICATE_FOLDER = f"event_certificate/{EVENT_NAME}" # Folder to store the certificates
+FONT_NAME = "Book-Antiqua" # Font Name
 
 def make_certificate(PER_NAME, FONT_NAME=FONT_NAME):
 
@@ -34,9 +40,10 @@ def make_certificate(PER_NAME, FONT_NAME=FONT_NAME):
         can.setFont(FONT_NAME, 40)  #Reduce the font size if the name is too long
     else:
         can.setFont(FONT_NAME, 54)
-    # can.setFont('Great Vibes 400', 54)
+
     fName = can.drawString(264, 345, PER_NAME)
     can.save()
+
     # move to the beginning of the StringIO buffer
     packet.seek(0)
 
@@ -49,24 +56,30 @@ def make_certificate(PER_NAME, FONT_NAME=FONT_NAME):
     page = existing_pdf.pages[0]
     page.merge_page(new_pdf.pages[0])
     output.add_page(page)
+
     # finally, write "output" to a real file
     # Create directory if it doesn't exist
     if not os.path.exists(CERTIFICATE_FOLDER):
         os.makedirs(CERTIFICATE_FOLDER)
 
-
-
     output_stream = open(os.path.join(CERTIFICATE_FOLDER, f"{PER_NAME}_{EVENT_NAME}.pdf"), "wb")
     output.write(output_stream)
     output_stream.close()
 
+
+# Test Mail
 # make_certificate(PER_NAME)
 # send_mail(EMAIL_ID, PER_NAME, EVENT_NAME, CERTIFICATE_FOLDER, CERTIFICATE)
-#
+
+
+# Read the excel file and automate certificate generation
 df = pd.read_excel(FILEPATH)
 name_list = df.to_dict(orient='records')
 
+
 for names in name_list:
+    # Ensure you have the correct column names in the excel file
+    # Example: "name" and "email_id"
     pdfGen = {'name': names['name'], 'email': names['email_id']}
 
     PER_NAME = pdfGen['name']
@@ -74,3 +87,5 @@ for names in name_list:
     make_certificate(PER_NAME)
     # send_mail(EMAIL_ID, PER_NAME, EVENT_NAME, CERTIFICATE_FOLDER, CERTIFICATE)
     print(PER_NAME)
+
+# It's generally a good practice to generate certificates first and then send mails 
